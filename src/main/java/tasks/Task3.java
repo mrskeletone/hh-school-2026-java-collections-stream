93528f84b7ce5e51cceed17f3304e91b56534921
mrskeletone <mr_skeletone_1000@mail.ru>
2026-02-21 22:53:15 +0300

    add task3

--- src/main/java/tasks/Task3.java
+++ src/main/java/tasks/Task3.java
@@ -1,9 +1,11 @@
 package tasks;
 
 import common.Person;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 Задача 3
@@ -12,6 +14,15 @@ import java.util.List;
 public class Task3 {
 
   public static List<Person> sort(Collection<Person> persons) {
-    return new ArrayList<>(persons);
+    return new ArrayList<>(persons).stream()
+        .sorted((o1, o2) -> {
+          if (o1.firstName().equals(o2.firstName()) && o1.secondName().equals(o2.secondName())) {
+            return o1.createdAt().compareTo(o2.createdAt());
+          } else if (o1.secondName().equals(o2.secondName())) {
+            return o1.firstName().compareTo(o2.firstName());
+          } else {
+            return o1.secondName().compareTo(o2.secondName());
+          }
+        }).collect(Collectors.toList());
   }
 }
