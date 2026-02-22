387d491fec016674b3a6194f6c061ef9ba63e619
mrskeletone <mr_skeletone_1000@mail.ru>
2026-02-22 13:21:04 +0300

    add task5

--- src/main/java/tasks/Task5.java
+++ src/main/java/tasks/Task5.java
@@ -3,9 +3,11 @@ package tasks;
 import common.ApiPersonDto;
 import common.Person;
 import common.PersonConverter;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
 Задача 5
@@ -23,6 +25,17 @@ public class Task5 {
   }
 
   public List<ApiPersonDto> convert(List<Person> persons, Map<Integer, Integer> personAreaIds) {
-    return new ArrayList<>();
+    return persons.stream()
+        .map(person -> convertPersonToApiPersonDto(person, personAreaIds.get(person.id())))
+        .collect(Collectors.toList());
+  }
+
+  public ApiPersonDto convertPersonToApiPersonDto(Person person, Integer personAreaIds) {
+    ApiPersonDto apiPersonDto = new ApiPersonDto();
+    apiPersonDto.setCreated(person.createdAt().toEpochMilli());
+    apiPersonDto.setId(person.id().toString());
+    apiPersonDto.setName(person.firstName());
+    apiPersonDto.setAreaId(personAreaIds);
+    return apiPersonDto;
   }
 }
