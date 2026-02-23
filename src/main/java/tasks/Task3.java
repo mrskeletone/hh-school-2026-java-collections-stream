f73064b82d6c99a81c095159838543150b3156c6
mrskeletone <mr_skeletone_1000@mail.ru>
2026-02-23 18:49:19 +0300

    fix task3

--- src/main/java/tasks/Task3.java
+++ src/main/java/tasks/Task3.java
@@ -4,6 +4,7 @@ import common.Person;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,14 +16,9 @@ public class Task3 {
 
   public static List<Person> sort(Collection<Person> persons) {
     return new ArrayList<>(persons).stream()
-        .sorted((o1, o2) -> {
-          if (o1.firstName().equals(o2.firstName()) && o1.secondName().equals(o2.secondName())) {
-            return o1.createdAt().compareTo(o2.createdAt());
-          } else if (o1.secondName().equals(o2.secondName())) {
-            return o1.firstName().compareTo(o2.firstName());
-          } else {
-            return o1.secondName().compareTo(o2.secondName());
-          }
-        }).collect(Collectors.toList());
+        .sorted(Comparator.comparing(Person::secondName,Comparator.nullsLast(String::compareTo))
+            .thenComparing(Person::firstName,Comparator.nullsLast(String::compareTo))
+            .thenComparing(Person::createdAt,Comparator.nullsLast(Comparator.naturalOrder())))
+        .collect(Collectors.toList());
   }
 }
